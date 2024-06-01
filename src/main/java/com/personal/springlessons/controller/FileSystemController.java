b747754724a054873f923d92e7e0ba16cac8dbf6
Denis Berretti <denis.berretti95@gmail.com>
2024-06-01 09:40:00 +0200

    Configurazione Jaeger + Prometheus

--- src/main/java/com/personal/springlessons/controller/FileSystemController.java
+++ src/main/java/com/personal/springlessons/controller/FileSystemController.java
@@ -4,14 +4,12 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-import io.micrometer.core.annotation.Timed;
 
 
 @RestController
 @RequestMapping(path = "/file-system")
 public class FileSystemController {
     
-    @Timed
     @GetMapping(path = "/ok")
     public ResponseEntity<String> ok() {
         return ResponseEntity.ok("OK");
