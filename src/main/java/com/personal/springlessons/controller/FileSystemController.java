a7e363ae2215dd9f9330e548fa64c47496a629a4
Denis Berretti <denis.berretti95@gmail.com>
2024-06-02 10:58:43 +0200

    Implementate API di test

--- src/main/java/com/personal/springlessons/controller/FileSystemController.java
+++ src/main/java/com/personal/springlessons/controller/FileSystemController.java
@@ -1,17 +1,27 @@
 package com.personal.springlessons.controller;
 
+import com.personal.springlessons.service.FileSystemService;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import lombok.RequiredArgsConstructor;
 
 
 @RestController
 @RequestMapping(path = "/file-system")
+@RequiredArgsConstructor
 public class FileSystemController {
-    
-    @GetMapping(path = "/ok")
-    public ResponseEntity<String> ok() {
-        return ResponseEntity.ok("OK");
+
+    private final FileSystemService fileSystemService;
+
+    @GetMapping(path = "/free-disk-space")
+    public ResponseEntity<Long> getFreeDiskSpace() {
+        return ResponseEntity.ok(fileSystemService.getFreeDiskSpace());
+    }
+
+    @GetMapping(path = "/total-space")
+    public ResponseEntity<Long> getTotalSpace() {
+        return ResponseEntity.ok(fileSystemService.getTotalSpace());
     }
 }
