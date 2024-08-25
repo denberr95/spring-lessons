aba5b262f7a28eca8c311424c8a5da7765bdb2db
Denis Berretti <denis.berretti95@gmail.com>
2024-08-25 20:28:38 +0200

    TODO

--- src/test/java/com/personal/springlessons/service/ItemsServiceTest.java
+++ src/test/java/com/personal/springlessons/service/ItemsServiceTest.java
@@ -1,9 +1,57 @@
 package com.personal.springlessons.service;
 
+import java.math.BigDecimal;
+import java.util.ArrayList;
+import java.util.List;
+import com.personal.springlessons.model.dto.ItemDTO;
+import com.personal.springlessons.model.dto.KafkaMessageItemDTO;
+import com.personal.springlessons.repository.IOrderItemsRepository;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.kafka.core.KafkaTemplate;
 
 @SpringBootTest
 class ItemsServiceTest {
 
-    // TODO
+    @Autowired
+    private ItemsService itemsService;
+
+    @Autowired
+    private IOrderItemsRepository orderItemsRepository;
+
+    @Autowired
+    private KafkaTemplate<String, KafkaMessageItemDTO> kafkaTemplate;
+
+    private static final int TOTAL = 5;
+    private List<ItemDTO> items;
+
+    @BeforeEach
+    void initAll() {
+        this.items = new ArrayList<>(TOTAL);
+        for (int i = 0; i < TOTAL; i++) {
+            ItemDTO itemDTO = new ItemDTO();
+            itemDTO.setBarcode("Service-Barcode-" + i);
+            itemDTO.setName("Service-Name-" + i);
+            itemDTO.setPrice(new BigDecimal(9_999.99));
+            this.items.add(itemDTO);
+        }
+    }
+
+    @AfterEach
+    void tearDown() {
+        // TODO
+    }
+
+    @Test
+    void testUpload() {
+        // TODO
+    }
+
+    @Test
+    void testDelete() {
+        // TODO
+    }
 }
