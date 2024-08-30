832e048bf84cf1b0ed126d2ae17a15c69a48b0b2
Denis Berretti <denis.berretti95@gmail.com>
2024-08-30 12:30:28 +0200

    Added print for info

--- container/keycloak/export-config.sh
+++ container/keycloak/export-config.sh
@@ -8,6 +8,8 @@ KC_DB_URL=jdbc:postgresql://keycloak-db:5432/keycloak
 KC_REALM_NAME_EXPORT=realm-export.json
 KC_REALM_NAME=realm.json
 
+
+echo "Export Keycloak configurations"
 podman exec auth-server $KC_DIR/bin/kc.sh export --file $KC_DIR/data/import/$KC_REALM_NAME_EXPORT --db-url=$KC_DB_URL --db-username=$KC_DB_USERNAME --db-password=$KC_DB_PWD --verbose || true
 
 if [[ -f "$KC_VOL_DIR/config/$KC_REALM_NAME_EXPORT" ]]; then
