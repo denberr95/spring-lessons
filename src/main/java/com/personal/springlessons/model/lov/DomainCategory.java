3bd67472f7794b3eab7aad803364f29582054929
Denis Berretti <denis.berretti95@gmail.com>
2024-08-31 09:16:57 +0200

    Improve msg e source code

--- src/main/java/com/personal/springlessons/model/lov/DomainCategory.java
+++ src/main/java/com/personal/springlessons/model/lov/DomainCategory.java
@@ -1,14 +1,16 @@
 package com.personal.springlessons.model.lov;
 
+import java.util.Arrays;
+import java.util.stream.Collectors;
 import com.fasterxml.jackson.annotation.JsonCreator;
 import com.fasterxml.jackson.annotation.JsonValue;
 import lombok.AllArgsConstructor;
 
 @AllArgsConstructor
 public enum DomainCategory {
-    BOOKS("Books"), ITEMS("Items"), ND("Not Defined");
+    BOOKS("Books"), ITEMS("Items"), ND("Not Defined"), AUTHORIZATION("Authorization");
 
-    private String value;
+    private final String value;
 
     @Override
     public String toString() {
@@ -21,12 +23,15 @@ public enum DomainCategory {
     }
 
     @JsonCreator
-    public static DomainCategory getValue(String value) {
+    public static DomainCategory fromValue(String value) {
         for (DomainCategory e : DomainCategory.values()) {
             if (e.value.equals(value)) {
                 return e;
             }
         }
-        throw new IllegalArgumentException(String.format("Unexpected value '%s'", value));
+        String allowedValues = Arrays.stream(DomainCategory.values()).map(DomainCategory::getValue)
+                .collect(Collectors.joining(", "));
+        throw new IllegalArgumentException(String
+                .format("Unexpected value '%s', allowed values are: '%s'", value, allowedValues));
     }
 }
