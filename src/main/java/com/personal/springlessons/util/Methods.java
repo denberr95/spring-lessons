49cdcaeee60e7825e3a97450b5f61e2a9c4cfbe5
Denis Berretti <denis.berretti95@gmail.com>
2024-08-31 09:16:32 +0200

    Aggiunti metodi di utility

--- src/main/java/com/personal/springlessons/util/Methods.java
+++ src/main/java/com/personal/springlessons/util/Methods.java
@@ -2,6 +2,7 @@ package com.personal.springlessons.util;
 
 import java.util.UUID;
 import com.personal.springlessons.exception.InvalidUUIDException;
+import com.personal.springlessons.model.lov.DomainCategory;
 import lombok.extern.slf4j.Slf4j;
 
 @Slf4j
@@ -21,6 +22,18 @@ public class Methods {
             throw new InvalidUUIDException(id);
         }
         log.debug("UUID: '{}' is valid", id);
+        return result;
+    }
+
+    public static DomainCategory retrieveDomainCategory(String uri) {
+        log.debug("Check Domain Category from uri: '{}'", uri);
+        DomainCategory result = DomainCategory.ND;
+        for (DomainCategory e : DomainCategory.values()) {
+            if (uri.toLowerCase().contains(e.getValue().toLowerCase())) {
+                result = e;
+            }
+        }
+        log.debug("Domain Category retrieved: '{}'", result);
         return result;
     }
 }
--- src/test/java/com/personal/springlessons/util/MethodsTest.java
+++ src/test/java/com/personal/springlessons/util/MethodsTest.java
@@ -6,6 +6,7 @@ import java.lang.reflect.Constructor;
 import java.lang.reflect.InvocationTargetException;
 import java.util.UUID;
 import com.personal.springlessons.exception.InvalidUUIDException;
+import com.personal.springlessons.model.lov.DomainCategory;
 import org.junit.jupiter.api.Test;
 
 class MethodsTest {
@@ -36,5 +37,17 @@ class MethodsTest {
         assertThrows(InvalidUUIDException.class, () -> {
             Methods.idValidation("fakeId");
         });
+    }
+
+    @Test
+    void giveValidUri_whenRetrieveDomainCategory_thenReturnSpecificDomainCategory() {
+        DomainCategory actual = Methods.retrieveDomainCategory( "/spring-app/books");
+        assertEquals(DomainCategory.BOOKS, actual);
+    }
+
+    @Test
+    void givenInvalidUri_whenRetrieveDomainCategory_thenReturnDefaultDomainCategory() {
+        DomainCategory actual = Methods.retrieveDomainCategory( "/fake-uri");
+        assertEquals(DomainCategory.ND, actual);
     }
 }
