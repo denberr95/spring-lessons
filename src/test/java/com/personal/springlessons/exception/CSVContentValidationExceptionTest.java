9fb2b831e055a412b521fd26de00b87d707fa45c
Denis Berretti <denis.berretti95@gmail.com>
2025-01-20 17:24:03 +0100

    Implemented UT

---
+++ src/test/java/com/personal/springlessons/exception/CSVContentValidationExceptionTest.java
@@ -0,0 +1,24 @@
+package com.personal.springlessons.exception;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import java.util.Arrays;
+import java.util.List;
+import com.personal.springlessons.model.dto.InvalidCsvDTO;
+import org.junit.jupiter.api.Test;
+
+class CSVContentValidationExceptionTest {
+
+    @Test
+    void testCSVContentValidationException() {
+        String fileName = "test.csv";
+        List<InvalidCsvDTO> rows = Arrays.asList(new InvalidCsvDTO(), new InvalidCsvDTO());
+
+        CSVContentValidationException exception = new CSVContentValidationException(fileName, rows);
+
+        assertNotNull(exception);
+        assertEquals(fileName, exception.getFileName());
+        assertEquals(rows, exception.getRows());
+        assertEquals("CSV file: test.csv is invalid", exception.getMessage());
+    }
+}
