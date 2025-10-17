f4fb72d06792d46d237cf9601baadbe2a73ad771
Denis Berretti <denis.berretti95@gmail.com>
2025-10-17 13:07:58 +0200

    ci(setup): adjusted hook setup

--- src/main/resources/db/migration/V3__create_table_books.sql
+++ src/main/resources/db/migration/V3__create_table_books.sql
@@ -1,12 +1,12 @@
 CREATE TABLE IF NOT EXISTS spring_app.books (
-    id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
-    created_at TIMESTAMP WITH TIME ZONE NOT NULL DEFAULT CLOCK_TIMESTAMP(),
-    updated_at TIMESTAMP WITH TIME ZONE,
-    version BIGINT NOT NULL DEFAULT 1,
-    name VARCHAR(100) NOT NULL,
-    publication_date DATE NOT NULL,
-    number_of_pages INTEGER NOT NULL,
-    genre TEXT NOT NULL DEFAULT 'NA',
-    channel TEXT NOT NULL DEFAULT 'NA',
-    CONSTRAINT unique_book UNIQUE (name, publication_date, number_of_pages)
+  id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
+  created_at TIMESTAMP WITH TIME ZONE NOT NULL DEFAULT clock_timestamp(),
+  updated_at TIMESTAMP WITH TIME ZONE,
+  version BIGINT NOT NULL DEFAULT 1,
+  name VARCHAR(100) NOT NULL,
+  publication_date DATE NOT NULL,
+  number_of_pages INTEGER NOT NULL,
+  genre TEXT NOT NULL DEFAULT 'NA',
+  channel TEXT NOT NULL DEFAULT 'NA',
+  CONSTRAINT unique_book UNIQUE (name, publication_date, number_of_pages)
 );
--- src/main/resources/db/migration/V5__create_table_items.sql
+++ src/main/resources/db/migration/V5__create_table_items.sql
@@ -1,11 +1,13 @@
 CREATE TABLE IF NOT EXISTS spring_app.items (
-    id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
-    created_at TIMESTAMP WITH TIME ZONE NOT NULL DEFAULT CLOCK_TIMESTAMP(),
-    updated_at TIMESTAMP WITH TIME ZONE,
-    price NUMERIC(6, 2) NOT NULL,
-    name VARCHAR(100) NOT NULL,
-    barcode VARCHAR(50) NOT NULL,
-    fk_order_items_id UUID NOT NULL,
-    CONSTRAINT unique_barcode UNIQUE (barcode),
-    CONSTRAINT fk_order_items_id FOREIGN KEY (fk_order_items_id) REFERENCES spring_app.order_items(id)
+  id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
+  created_at TIMESTAMP WITH TIME ZONE NOT NULL DEFAULT clock_timestamp(),
+  updated_at TIMESTAMP WITH TIME ZONE,
+  price NUMERIC(6, 2) NOT NULL,
+  name VARCHAR(100) NOT NULL,
+  barcode VARCHAR(50) NOT NULL,
+  fk_order_items_id UUID NOT NULL,
+  CONSTRAINT unique_barcode UNIQUE (barcode),
+  CONSTRAINT fk_order_items_id FOREIGN KEY (
+    fk_order_items_id
+  ) REFERENCES spring_app.order_items (id)
 );
