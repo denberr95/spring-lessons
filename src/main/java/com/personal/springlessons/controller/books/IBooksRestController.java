117d8f1bf519f3b6631ea051155b4ae5c5912ec6
Denis Berretti <denis.berretti95@gmail.com>
2026-02-09 16:29:06 +0100

    fix(api): fixed springdoc documentation

--- src/main/java/com/personal/springlessons/controller/books/IBooksRestController.java
+++ src/main/java/com/personal/springlessons/controller/books/IBooksRestController.java
@@ -1,9 +1,7 @@
 package com.personal.springlessons.controller.books;
 
 import java.util.List;
-
 import jakarta.validation.Valid;
-
 import com.personal.springlessons.model.dto.BookDTO;
 import com.personal.springlessons.model.dto.response.BookNotFoundResponseDTO;
 import com.personal.springlessons.model.dto.response.ConcurrentUpdateResponseDTO;
@@ -17,7 +15,7 @@ import com.personal.springlessons.model.dto.response.MissingHttpRequestHeaderRes
 import com.personal.springlessons.model.dto.response.NotReadableBodyRequestResponseDTO;
 import com.personal.springlessons.model.dto.response.ValidationRequestErrorResponseDTO;
 import com.personal.springlessons.model.lov.Channel;
-
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.annotation.Validated;
@@ -31,7 +29,6 @@ import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestPart;
 import org.springframework.web.multipart.MultipartFile;
-
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.media.ArraySchema;
 import io.swagger.v3.oas.annotations.media.Content;
@@ -58,7 +55,7 @@ public interface IBooksRestController {
   ResponseEntity<List<BookDTO>> getAll();
 
   @SecurityRequirement(name = "oauth2", scopes = "books:get")
-  @GetMapping(path = "{id}", produces = MediaType.APPLICATION_JSON_VALUE)
+  @GetMapping(path = "/{id}", produces = MediaType.APPLICATION_JSON_VALUE)
   @Operation(summary = "Get a book by id", operationId = "getBookByIdV1")
   @ApiResponse(responseCode = "200", description = "OK",
       content = {@Content(schema = @Schema(implementation = BookDTO.class))})
@@ -71,7 +68,8 @@ public interface IBooksRestController {
   ResponseEntity<BookDTO> getById(@PathVariable final String id);
 
   @SecurityRequirement(name = "oauth2", scopes = "books:create")
-  @PostMapping(produces = MediaType.APPLICATION_JSON_VALUE)
+  @PostMapping(produces = MediaType.APPLICATION_JSON_VALUE,
+      consumes = MediaType.APPLICATION_JSON_VALUE)
   @Operation(summary = "Create a new book", operationId = "createBookV1")
   @ApiResponse(responseCode = "201", description = "Created",
       content = {@Content(schema = @Schema(implementation = BookDTO.class))})
@@ -87,12 +85,14 @@ public interface IBooksRestController {
       @RequestHeader final Channel channel);
 
   @SecurityRequirement(name = "oauth2", scopes = "books:delete")
-  @DeleteMapping(path = "{id}", produces = MediaType.APPLICATION_JSON_VALUE)
+  @DeleteMapping(path = "/{id}", produces = MediaType.APPLICATION_JSON_VALUE)
   @Operation(summary = "Delete a book", operationId = "deleteBookV1")
   @ApiResponse(responseCode = "204", description = "No Content",
       content = {@Content(schema = @Schema(implementation = Void.class))})
   @ApiResponse(responseCode = "400", description = "Bad Request",
-      content = {@Content(schema = @Schema(implementation = InvalidUUIDResponseDTO.class))})
+      content = {@Content(schema = @Schema(oneOf = {InvalidUUIDResponseDTO.class,
+          MissingHttpRequestHeaderResponseDTO.class, InvalidArgumentTypeResponseDTO.class,
+          ValidationRequestErrorResponseDTO.class, NotReadableBodyRequestResponseDTO.class}))})
   @ApiResponse(responseCode = "404", description = "Not Found",
       content = {@Content(schema = @Schema(implementation = BookNotFoundResponseDTO.class))})
   @ApiResponse(responseCode = "409", description = "Conflict",
@@ -100,10 +100,11 @@ public interface IBooksRestController {
   @ApiResponse(responseCode = "500", description = "Internal Server Error",
       content = {@Content(schema = @Schema(implementation = GenericErrorResponseDTO.class))})
   ResponseEntity<Void> delete(@PathVariable final String id,
-      @RequestHeader(value = "If-Match") final String ifMatch);
+      @RequestHeader(HttpHeaders.IF_MATCH) final String ifMatch);
 
   @SecurityRequirement(name = "oauth2", scopes = "books:update")
-  @PutMapping(path = "{id}", produces = MediaType.APPLICATION_JSON_VALUE)
+  @PutMapping(path = "/{id}", produces = MediaType.APPLICATION_JSON_VALUE,
+      consumes = MediaType.APPLICATION_JSON_VALUE)
   @Operation(summary = "Update a book", operationId = "updateBookV1")
   @ApiResponse(responseCode = "200", description = "OK",
       content = {@Content(schema = @Schema(implementation = BookDTO.class))})
@@ -120,10 +121,10 @@ public interface IBooksRestController {
       content = {@Content(schema = @Schema(implementation = GenericErrorResponseDTO.class))})
   ResponseEntity<BookDTO> update(@PathVariable final String id,
       @Valid @RequestBody final BookDTO bookDTO, @RequestHeader final Channel channel,
-      @RequestHeader(value = "If-Match") final String ifMatch);
+      @RequestHeader(HttpHeaders.IF_MATCH) final String ifMatch);
 
   @SecurityRequirement(name = "oauth2", scopes = "books:download")
-  @GetMapping(path = "download")
+  @GetMapping(path = "/download", produces = MediaType.APPLICATION_OCTET_STREAM_VALUE)
   @Operation(summary = "Download books in a csv file", operationId = "downloadBooksV1")
   @ApiResponse(responseCode = "200", description = "OK",
       content = {@Content(schema = @Schema(implementation = byte.class),
@@ -134,7 +135,7 @@ public interface IBooksRestController {
   ResponseEntity<byte[]> download();
 
   @SecurityRequirement(name = "oauth2", scopes = "books:upload")
-  @PostMapping(path = "upload", produces = MediaType.APPLICATION_JSON_VALUE,
+  @PostMapping(path = "/upload", produces = MediaType.APPLICATION_JSON_VALUE,
       consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
   @Operation(summary = "Upload books from a csv file", operationId = "uploadBooksV1")
   @ApiResponse(responseCode = "204", description = "No Content",
