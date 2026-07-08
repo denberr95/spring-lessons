8e31071d70b2428889dab0cf25ef05dcace6fb50
Denis Berretti <denis.berretti95@gmail.com>
2026-07-08 19:40:27 +0200

    fix(database): align books genre and channel column type with audit

--- src/main/resources/db/migration/V3__create_table_books.sql
+++ src/main/resources/db/migration/V3__create_table_books.sql
@@ -6,8 +6,8 @@ CREATE TABLE IF NOT EXISTS spring_app.books (
   name VARCHAR(100) NOT NULL,
   publication_date DATE NOT NULL,
   number_of_pages INTEGER NOT NULL,
-  genre TEXT NOT NULL DEFAULT 'NA',
-  channel TEXT NOT NULL DEFAULT 'NA',
+  genre VARCHAR(50) NOT NULL DEFAULT 'NA',
+  channel VARCHAR(50) NOT NULL DEFAULT 'NA',
   CONSTRAINT pk_books_id PRIMARY KEY (id),
   CONSTRAINT uk_books_name_pub_date_pages UNIQUE (name, publication_date, number_of_pages)
 );
